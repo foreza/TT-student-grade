1a1abc5cadd3fd77509fb4ccaa03ae4205b4fc4b
Jason Chiu <jforeza@gmail.com>
2019-12-21 17:41:04 -0800

    Support addition of new rows
    Changed sample data set to start from 0 based indexing
    Added ids to the footer input form to be able to grab value
    Moved rendering function to a helper method that will return the new object instead
    Rendering helper function will be called on page load and on new object addition
    Clear input form after submission

--- js/studentGrades.js
+++ js/studentGrades.js
@@ -1,16 +1,61 @@
 
 // Sample Array of student Grades
 const students = [
-    {id: 1, name: 'Do', grade: 24},
-    {id: 2, name: 'Rey', grade: 23},
-    {id: 3, name: 'Mee', grade: 99},
-    {id: 4, name: 'Fa', grade: 88},
+    {id: 0, name: 'Do', grade: 24},
+    {id: 1, name: 'Rey', grade: 23},
+    {id: 2, name: 'Mee', grade: 99},
+    {id: 3, name: 'Fa', grade: 88},
     ];
 
 // Maintain reference to the tBody 
 const tbody = document.querySelector('#table-grades tbody');
 
+// Maintain reference to the input form 
+const tFooterNameInput = document.querySelector('#input-name');
+const tFooterGradeInput = document.querySelector('#input-grade');
+
 renderStudentGradeTable(students);
+
+
+
+
+
+
+
+
+// Functions
+
+
+function addNewStudentToArray(studentObj){
+
+    students.push(studentObj);      // Add it to the actual array
+
+    // Update view. This is the naive approach; change this later
+    tbody.appendChild(returnCreatedRowItemForStudent(studentObj));
+
+
+}
+
+
+function attemptAddNewRow(){
+
+// TODO: Do some form validation here as well
+
+    let newStudent = {
+        id: students.length,    // TODO: How are we assigning IDs?
+        name: tFooterNameInput.value,
+        grade: tFooterGradeInput.value
+    }
+
+    addNewStudentToArray(newStudent);
+
+    // Clear input after
+
+    tFooterNameInput.value = "";
+    tFooterGradeInput.value = "";
+
+}
+
 
 // TODO: Implement edit function
 function editRowWithStudentID(id){
@@ -22,62 +67,65 @@ function deleteRowWithStudentID(id){
 }
 
 
-// Function to render grades given a 
+// Helper function that returns a new row object
+function returnCreatedRowItemForStudent(student){
+
+    const row = document.createElement('tr');   
+
+    const colName = document.createElement('td');
+    const colGrade = document.createElement('td');
+    const colOptions = document.createElement('td');
+
+    colName.textContent = student.name;
+    colGrade.textContent = student.grade;
+
+    const onHoverShow = document.createElement('div');
+    onHoverShow.setAttribute('class', 'on-hover-show');
+    
+    const dropDownButton = document.createElement('button');
+    dropDownButton.textContent = "...";
+
+    const dropDownContent = document.createElement('div');
+    dropDownContent.setAttribute('class', 'dropdown-content');
+
+    const editLink = document.createElement('a');
+    const deleteLink = document.createElement('a');
+
+    editLink.setAttribute('href', "#");
+    deleteLink.setAttribute('href', "#");
+
+    // TODO: Fix this up later
+    editLink.setAttribute('onclick', `editRowWithStudentID(${student.id})`);
+    deleteLink.setAttribute('onclick', `deleteRowWithStudentID(${student.id})`);
+
+    editLink.textContent = 'Edit';
+    deleteLink.textContent = 'Delete';
+
+    dropDownContent.appendChild(editLink);
+    dropDownContent.appendChild(deleteLink);
+
+    
+    onHoverShow.appendChild(dropDownButton);
+    onHoverShow.appendChild(dropDownContent);
+
+    colOptions.appendChild(onHoverShow);
+
+
+    row.setAttribute('id', student.id);
+    row.appendChild(colName);
+    row.appendChild(colGrade);
+    row.appendChild(colOptions);
+
+    return row;
+
+}
+
+
+// Function to render grades 
 function renderStudentGradeTable(studentGradeList) {
 
-
     studentGradeList.forEach(student => {
-
-        const row = document.createElement('tr');
-        const colName = document.createElement('td');
-        const colGrade = document.createElement('td');
-        const colOptions = document.createElement('td');
-
-
-        colName.textContent = student.name;
-        colGrade.textContent = student.grade;
-
-        const onHoverShow = document.createElement('div');
-        onHoverShow.setAttribute('class', 'on-hover-show');
-        
-        const dropDownButton = document.createElement('button');
-        dropDownButton.textContent = "...";
-
-        const dropDownContent = document.createElement('div');
-        dropDownContent.setAttribute('class', 'dropdown-content');
-
-        const editLink = document.createElement('a');
-        const deleteLink = document.createElement('a');
-
-        editLink.setAttribute('href', "#");
-        deleteLink.setAttribute('href', "#");
-
-        // TODO: Fix this up later
-        editLink.setAttribute('onclick', `editRowWithStudentID(${student.id})`);
-        deleteLink.setAttribute('onclick', `deleteRowWithStudentID(${student.id})`);
-
-        editLink.textContent = 'Edit';
-        deleteLink.textContent = 'Delete';
-
-        dropDownContent.appendChild(editLink);
-        dropDownContent.appendChild(deleteLink);
-
-        
-        onHoverShow.appendChild(dropDownButton);
-        onHoverShow.appendChild(dropDownContent);
-
-        colOptions.appendChild(onHoverShow);
-
-
-        row.setAttribute('id', student.id);
-        row.appendChild(colName);
-        row.appendChild(colGrade);
-        row.appendChild(colOptions);
-
-        tbody.appendChild(row);
-
+        tbody.appendChild(returnCreatedRowItemForStudent(student));
         });
-
-
 
 }
