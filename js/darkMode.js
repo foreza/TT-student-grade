5678c0db48aeabbff58861d36040b8551a51cbf9
Jason Chiu <jforeza@gmail.com>
2020-01-06 23:59:42 -0800

    Update darkMode to use jQuery and bind event listeners, prepare to use for localstorage

--- js/darkMode.js
+++ js/darkMode.js
@@ -1,15 +1,43 @@
-let isDarkMode = false;         // T = dark mode, F = light mode.
+var isDarkMode = false;         // Temporary value
 
-function toggleDarkMode() {
 
-  if (!isDarkMode) {
-    document.getElementById("body-content")
-      .classList.add('darkMode');
-    isDarkMode = true;
+// On document ready, set the dark mode theme
+$(document).ready(function () {
+
+  isDarkMode = getDarkModeState();      // Set initial state
+  setDarkMode(isDarkMode);              
+
+  // Bind event listener to the button
+  $("#darkModeToggle").bind({
+    click: function () {
+      toggleDarkModeState();    // Toggle the value
+      setDarkMode(isDarkMode);  // Update the style
+    }
+  });
+
+});
+
+
+// Function to retrieve the state (TODO: implement from local / remote)
+function getDarkModeState() {
+  return isDarkMode;
+}
+
+
+// Update DOM to use darkMode style or remove it
+function setDarkMode(value) {
+
+  if (value) {
+    $('#body-content').attr('class', 'darkMode');
   } else {
-    document.getElementById("body-content")
-      .classList.remove('darkMode');
-    isDarkMode = false;
+    $('#body-content').removeAttr('class', 'darkMode');
   }
 
+
+}
+
+
+// Toggle (and TODO: save) from local / remote
+function toggleDarkModeState(){
+  isDarkMode = !isDarkMode;
 }
